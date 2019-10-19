799f8e37d2c17f5ec47a409ef04436c71593d784
Matthew Carroll <matthew.j.carroll@outlook.com>
2019-10-18 19:36:56 -0500

    Started simple CNN class

--- neural_net/cnn.py
+++ neural_net/cnn.py
@@ -1,13 +1,23 @@
 import numpy as np
 from PIL import Image
-import torch
+from net import Net
+
+training_data = []
+weights = []
 
 def load_data(filename):
     f = open(filename, "r")
     for line in f:
         im = Image.open(line)
+        matrix = np.array(im)
+        matrix = ((matrix[0] * matrix[1]), matrix[2])
+        training_data.append(matrix)
     f.close()
 
 def train_cnn():
+    training_data 
 
 def initialize_weights():
+    n = Net(training_data[0].shape)
+    for x in training_data:
+        n.forward(x)
