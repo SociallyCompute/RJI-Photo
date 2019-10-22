33bfa3fa9e3c71f7e83442e7e600a7b7facafbc0
Matthew Carroll <matthew.j.carroll@outlook.com>
2019-10-22 16:11:24 -0500

    updated run_files to look through edited photos

--- cluster/run_files.py
+++ cluster/run_files.py
@@ -27,28 +27,21 @@ def print_menu():
 def run(root, halt, alg):
     count = 0
     for(loc,dirs,files) in os.walk(root,topdown=True):
-        # print(loc)
-        # print(dirs)
-        # print(files)
-        # print('-------')
-        # if(loc == (root + '/' + halt)):
-        #     break
         for f in files:
             if(f.lower().endswith('.jpg')):
                 im = Image.open(loc + '/' + f)
-                fl = open("22080files.txt", "a")
-                fi = open("18048files.txt", "a")
-                fil = open("9024files.txt", "a")
+                fl = open("edited22080files.txt", "a")
+                fi = open("edited18048files.txt", "a")
+                fil = open("edited9024files.txt", "a")
                 #this should never fire, it would mean there is a duplicate picture
                 if(f in pics):
                     return
                 #add to pics dictionary with file name for key and add to np list
                 # im = im.convert('1') #convert to grayscale
                 mat2d = np.array(im)
-                #print(mat3d.shape)
+                print(mat2d.shape)
                 if(mat2d.ndim == 3):
                     mat2d = mat2d.reshape((mat2d.shape[1] * mat2d.shape[2]), mat2d.shape[0])
-                print(mat2d.shape)
                 if(mat2d.shape[0] == 22080):
                     print("test")
                     fl.write(str(loc) + "/" + str(f) + "\n")
@@ -58,30 +51,18 @@ def run(root, halt, alg):
                 elif(mat2d.shape[0] == 9024):
                     print('test3')
                     fil.write(str(loc) + "/" + str(f) + "\n")
-
+                else:
+                    print(mat2d.shape)
                 count = count + 1
-                #if (mat2d.shape[1] == 22080 or mat2d.shape[0] == 22080):
-                    #print(files)
-                    #print('-------')
-                    #cluster.add_to_list(loc,f)
-                    #count = count + 1
-                #else:
-                   # break
-        #if(count >= 250):
-            #print(count)
-            #break
     f.close()
-    print(count)
-    n_comp = input("How many n_components would you like to compress: ")
-    cluster.pca_compress(int(n_comp)) #param passed is n_components compressed in PCA
-    if(alg == "1" or alg == "K-Means"):
-        n_clus = input("How many clusters would you like to group: ")
-        cluster.run_kmeans(int(n_clus)) #param is n_clusters in kmeans
-    elif(alg == "2" or alg == "KNN"):
-        n_clus = input("How many clusters would you like to group: ")
-        cluster.run_knn(n_clus)
-    else:
-        print("No functionality provided for the algorithm yet")
+    # print(count)
+    # cluster.pca_compress(int(input("How many n_components would you like to compress: "))) #param passed is n_components compressed in PCA
+    # if(alg == "1" or alg == "K-Means"):
+    #     cluster.run_kmeans(int(input("How many clusters would you like to group: "))) #param is n_clusters in kmeans
+    # elif(alg == "2" or alg == "KNN"):
+    #     cluster.run_knn(int(input("How many clusters would you like to group: "))
+    # else:
+    #     print("No functionality provided for the algorithm yet")
 
 def run_files():
     count = 0
@@ -106,16 +87,14 @@ def run_files():
 #have argv[1] be the root folder and argv[2] be the first folder we don't want to pull from
 if(__name__ == "__main__"):
     alg, data, stop = print_menu()
-    # print(alg)
-    # print(data)
-    # print(stop)
     #how can I generalize this without requiring people type this out?
     if(data == "root" or data == '1'):
-        root_dir = '/mnt/md0/mysql-dump-economists/Archives/2017/Fall/Dump/'
+        # root_dir = '/mnt/md0/mysql-dump-economists/Archives/2017/Fall/Dump/'
+        root_dir = '/mnt/md0/mysql-dump-economists/Archives/2017/Fall/Edited/'
     else:
         root_dir = data
     
-    if alg == "1":
-        run_files()
-    else:
-        run(root_dir, stop, alg)
+    # if alg == "1":
+    #     run_files()
+    # else:
+    run(root_dir, stop, alg)
