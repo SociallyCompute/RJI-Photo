a9fb735a33d336ff6a39770edd64a63d81dded20
Matthew Carroll <matthew.j.carroll@outlook.com>
2019-12-23 09:39:31 -0600

    fixing labels as tensors

--- metadata/pull_xmp_file.py
+++ metadata/pull_xmp_file.py
@@ -18,11 +18,11 @@ from torchvision import datasets, transforms
 
 def find_color_code(data_loader):
     # creating an array of 1000 inputs to map to the one hot encoding, 1 is high 0 is low
-    base_label = np.zeros(1000)
+    # base_label = np.zeros(1000)
     counter = 0
     i = 0
     for _,label,paths in data_loader:
-        print(label)
+        # print(label)
         i = i+1
         path=paths[0]
         # print(path)
@@ -40,28 +40,28 @@ def find_color_code(data_loader):
             # labels[path.decode('ascii')] = xmp_string[26]
         # print(label)
             if(xmp_string[26] == '1'):
-                base_label[999] = 1
-                label = torch.tensor(base_label)
-                base_label[999] = 0
+                # base_label[999] = 1
+                label = torch.tensor([999])
+                # base_label[999] = 0
             elif(xmp_string[26] == '2'):
-                base_label[800] = 1
-                label = torch.tensor(base_label)
-                base_label[800] = 0
+                # base_label[800] = 1
+                label = torch.tensor([800])
+                # base_label[800] = 0
             elif(xmp_string[26] == '3'):
-                base_label[700] = 1
-                label = torch.tensor(base_label)
-                base_label[700] = 0
+                # base_label[700] = 1
+                label = torch.tensor([700])
+                # base_label[700] = 0
             elif(xmp_string[26] == '4'):
-                base_label[650] = 1
-                label = torch.tensor(base_label)
-                base_label[650] = 0
+                # base_label[650] = 1
+                label = torch.tensor([650])
+                # base_label[650] = 0
             elif(xmp_string[26] == '5'):
-                base_label[500] = 1
-                label = torch.tensor(base_label)
-                base_label[500] = 0
+                # base_label[500] = 1
+                label = torch.tensor([500])
+                # base_label[500] = 0
             else:
-                base_label[250] = 1
-                label = torch.tensor(base_label)
-                base_label[250] = 0
+                # base_label[250] = 1
+                label = torch.tensor([250])
+                # base_label[250] = 0
     print(counter)
     print("Total Images: " + str(i))
