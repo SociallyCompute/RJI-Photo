f3a9a844a8dcfac0175d21bd3a4db6652fac0fc8
Matthew Carroll <matthew.j.carroll@outlook.com>
2020-03-18 11:14:33 -0500

    cleaning repo from comments and unnecessary code

--- background_tasks/run_mudcats.sh
+++ background_tasks/run_mudcats.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python model_builder.py Mar16_AVA_MINI256_resnet AVA 15 256 resnet &> model_builder.out & 
+nohup python model_builder.py Mar18_Missourian_1_MINI256_resnet Missourian 1 256 resnet &> Mar18_model_builder.out & 
