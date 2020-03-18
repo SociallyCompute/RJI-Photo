3fd94e44f1cd1c0712484bfbbbb98fcb39f83f22
Matthew Carroll <matthew.j.carroll@outlook.com>
2020-03-18 16:08:35 +0000

    adding Missourian label text files to repo

--- background_tasks/run_mudcats.sh
+++ background_tasks/run_mudcats.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-model_name=Mar13_Missourian_MINI256
-dataset=Missourian
-epochs=1
-batch_size=256
-model=resnet
-
-
-echo "Starting $model model builder"
-
-nohup python model_builder.py $model_name $datset $epochs $batch_size $model &> model_builder.out & 
+nohup python model_builder.py Mar16_AVA_MINI256_resnet AVA 15 256 resnet &> model_builder.out & 
