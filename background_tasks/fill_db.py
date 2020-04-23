4d57e5d8fe01678011735d005fef33d218e3da7f
Matthew Carroll <matthew.j.carroll@outlook.com>
2020-04-23 12:09:14 -0500

    changed helpers.py to misc.py to avoid python module clashing

--- background_tasks/fill_db.py
+++ background_tasks/fill_db.py
@@ -1,8 +1,9 @@
 import sys
-from ..common import helpers
+# from ..common import helpers
+from common import misc
 import logging
 
 logging.basicConfig(filename='logs/fill_db.log', 
                     filemode='w', level=logging.DEBUG)
 
-helpers.write_xmp_color_class()
+misc.write_xmp_color_class()
