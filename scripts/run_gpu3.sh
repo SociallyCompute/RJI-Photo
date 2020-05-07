ba8070d3ba863f2e8bd5028f747d6c919baaeb44
Matthew Carroll <matthew.j.carroll@outlook.com>
2020-05-07 15:03:37 -0500

    Upped expected GPU memory and reduced batch size

--- scripts/run_gpu3.sh
+++ scripts/run_gpu3.sh
@@ -5,7 +5,7 @@
 ## resources
 #SBATCH --partition gpu3
 #SBATCH --cpus-per-task=1  # cores per task
-#SBATCH --mem-per-cpu=16G  # memory per core (default is 1GB/core)
+#SBATCH --mem-per-cpu=32G  # memory per core (default is 1GB/core)
 #SBATCH --time 1-23:00     # days-hours:minutes
 #SBATCH --account=general-gpu  # investors will replace this (e.g. `rc-gpu`)
 #SBATCH --gres gpu:1
@@ -25,10 +25,10 @@ echo "### Starting at: $(date) ###"
 module load cudnn/cudnn-7.1.4-cuda-9.0.176
 
 # Science goes here:
-modelname='May4_ava_30ep_MINI32_resnet'
+modelname='May7_ava_30ep_MINI16_resnet'
 dataset='ava'
 epochs='30'
-batch='32'
+batch='16'
 architecture='resnet'
 classification='content'
 
