c8e968ecf1996e3f9db0910918377b5e8f98c824
Matthew Carroll <matthew.j.carroll@outlook.com>
2021-03-06 12:34:53 -0600

    adding initial website code and changing to regression model

--- common/preprocess.py
+++ common/preprocess.py
@@ -2,6 +2,7 @@ from PIL import Image
 import os, sys
 import numpy as np
 import pandas as pds
+from torchvision import transforms
 
 path_to_images = '/media/matt/New Volume/ava/ava-compressed/images'
 path_to_labels = '/media/matt/New Volume/ava/AVA.txt'
@@ -15,9 +16,27 @@ def convert_dataset():
     for _,f in enumerate(os.listdir(path_to_images)):
         try:
             idx = int(f.split('.')[0])
-            img_array = np.array(Image.open(path_to_images + '/' + f))
+            img = Image.open(path_to_images + '/' + f)
+            img = img.convert('RGB')
+            transform = transforms.Compose([transforms.Resize(256),
+                            transforms.CenterCrop(224),
+                            transforms.ToTensor(),
+                            # transforms.Normalize(
+                            #     mean=[0.5],
+                            #     std=[0.5])
+                            # ])
+                            transforms.Normalize(
+                                mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+                            ])
+            img_array = np.array(transform(img))
             lab_array = np.array((labels.loc[idx])[1:11])
-            np.savez('/media/matt/New Volume/ava/np_files/' + str(idx), x=img_array, y=lab_array)
+            result = 0
+            for i in range(len(lab_array)):
+                result += i*lab_array[i]
+            result = result/np.sum(lab_array)
+            lab_array = np.array(result)
+            np.savez('/media/matt/New Volume/ava/np_regress_files/' + str(idx), x=img_array, y=lab_array)
         except Exception as ex:
             print(f)
             print(ex)
--- common/testing_dataset_model.py
+++ common/testing_dataset_model.py
@@ -6,6 +6,7 @@ from PIL import Image
 from torch.utils.data import Dataset, DataLoader, TensorDataset, ConcatDataset
 import matplotlib.pyplot as plt
 from skimage import transform
+from sklearn.metrics import accuracy_score
 
 import os
 import time
@@ -98,6 +99,7 @@ This is done exclusively to have a baseline model for ResNet for verification pu
 """
 class baselineResNet(nn.Module):
     def __init__(self,pretrain=True,frozen=True):
+        super(baselineResNet, self).__init__()
         self.resnet = models.resnet50(pretrained=pretrain)
         if(frozen):
             for param in self.resnet.parameters():
@@ -112,6 +114,7 @@ Our modified version with changing the final layer (using CNN as classifier)
 """
 class modifiedResNet(nn.Module):
     def __init__(self,num_outputs,existing_model=None,frozen=False):
+        super(modifiedResNet, self).__init__()
         self.num_outputs = num_outputs
         self.existing_model = existing_model
         self.resnet = models.resnet50(pretrained=True)
@@ -121,75 +124,8 @@ class modifiedResNet(nn.Module):
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_outputs)
 
     def forward(self, x):
-        out = x
+        out = self.resnet(x)
         return out
-
-"""
-OLD Train method
-"""
-# def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-#     since = time.time()
-
-#     best_model_wts = copy.deepcopy(model.state_dict())
-#     best_loss = 1000.0
-
-#     for epoch in range(num_epochs):
-#         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-#         print('-' * 10)
-
-#         model.train()  # Set model to training mode
-
-#         running_loss = 0.0
-#         running_corrects = 0
-
-#         # Iterate over data.
-#         for batch in dataloader:
-#             inputs = batch['image'].to(device)
-#             labels = torch.flatten(batch['classes'].to(device), start_dim=1)
-
-#             # zero the parameter gradients
-#             optimizer.zero_grad()
-
-#             # forward
-#             # track history if only in train
-#             with torch.set_grad_enabled(True):
-#                 outputs = model(inputs)
-#                 output = torch.max(outputs)
-#                 ans  = torch.max(labels).float()
-#                 # print(output)
-#                 # print(ans)
-#                 loss = criterion(output, ans)
-
-#                 # backward + optimize only if in training phase
-#                 if True:
-#                     loss.backward()
-#                     optimizer.step()
-
-#             # statistics
-#             running_loss += loss.item() * inputs.size(0)
-#             # running_corrects += torch.sum(preds == ans)
-#         if True:
-#             scheduler.step()
-
-#         epoch_loss = running_loss / dataset_size
-#         # epoch_acc = running_corrects.double() / dataset_size
-
-#         print('{} Loss: {:.4f}'.format(
-#             "train", epoch_loss))
-
-#         # deep copy the model
-#         if epoch_loss < best_loss:
-#             best_loss = epoch_loss
-#             best_model_wts = copy.deepcopy(model.state_dict())
-
-#     time_elapsed = time.time() - since
-#     print('Training complete in {:.0f}m {:.0f}s'.format(
-#         time_elapsed // 60, time_elapsed % 60))
-#     print('Best Loss: {:4f}'.format(best_loss))
-
-#     # load best model weights
-#     model.load_state_dict(best_model_wts)
-#     return model
 
 """
 NEW Train method
@@ -202,7 +138,7 @@ def train_model(model,save_filepath,training_loader,validation_loader):
     data_loaders = {"train": training_loader, "val": validation_loader}
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    loss_func = nn.MSELoss()
+    loss_func = nn.MSELoss() #nn.CrossEntropyLoss()
 
     # training and testing
     for epoch in range(100):
@@ -217,9 +153,16 @@ def train_model(model,save_filepath,training_loader,validation_loader):
 
             running_loss = 0.0
             for i, (x, y) in enumerate(data_loaders[phase]):       
-    
-                output = model(x)                              
-                loss = loss_func(output, y)                  
+                # print(x.dtype)
+                output = model(x)   
+                # print(torch.squeeze(output))
+                # print(y)  
+                # print("output: " + str(torch.argmax(output, dim=1)))
+                # print(accuracy_score(output.detach().cpu().numpy(), y.detach().cpu().numpy()))
+                # print("labels: " + str(torch.argmax(y, dim=1)))
+                # loss = loss_func(output, torch.argmax(y, dim=1))
+                loss = loss_func(torch.squeeze(output).type(torch.FloatTensor), torch.argmax(y).type(torch.FloatTensor))
+                # print(loss.dtype)
                 optimizer.zero_grad()           
 
                 if phase == 'train':
@@ -283,90 +226,35 @@ def visualize_model(model, num_images=64):
                     return
         model.train(mode=was_training)
 
-# ava_dataset = AVAImagesDataset(csv_file='/media/matt/New Volume/ava/cleanedlabels.txt', 
-#                                root_dir='/media/matt/New Volume/ava/ava-compressed/images',
-#                                transform=transforms.Compose([
-#                                    transforms.Resize(256), 
-#                                    transforms.CenterCrop(224), 
-#                                    transforms.ToTensor(), 
-#                                    transforms.Normalize(
-#                                        mean=[0.485, 0.456, 0.406], 
-#                                        std=[0.229, 0.224, 0.225])
-#                                        ]))
+def get_dataset(data_filepath, idx):
+    dataset_list = []
+    for i,f in enumerate(os.listdir(data_filepath), idx):
+        data = np.load(data_filepath + f)
+        if (i - idx) > 1023:
+            break
+        tensor_x = torch.Tensor(data['x'])
+        tensor_x = torch.unsqueeze(tensor_x, 0)
+        # print(tensor_x.size())
+        tensor_y = torch.Tensor(data['y'])
+        tensor_y = torch.unsqueeze(tensor_y, 0)
+        # print(tensor_y.size())
 
-# dataset_size = len(ava_dataset)
+        dataset_list.append(TensorDataset(tensor_x,tensor_y))
 
-# dataloader = DataLoader(ava_dataset, batch_size=1)
-
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
-# # Get a batch of training data
-# batch = next(iter(dataloader))
-# # print(batch['classes'])
-
-# # Make a grid from batch
-# out = torchvision.utils.make_grid(batch['image'])
-
-# model_ft = models.resnet50(pretrained=True)
-# for param in model_ft.parameters():
-#     param.requires_grad = False
-# num_ftrs = model_ft.fc.in_features
-# # Here the size of each output sample is set to 2.
-# # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-# # model_ft.fc = nn.Linear(num_ftrs, 10)
-# model_ft.fc = nn.Linear(num_ftrs, 1)
-
-# # model_ft.load_state_dict(torch.load("Nov17_r50_6ep_32batch.pt"))
-# # for param in model_ft.parameters():
-#     # param.requires_grad = True
-
-# model_ft = model_ft.to(device)
-
-# criterion = nn.MSELoss()
-
-# # Observe that all parameters are being optimized
-# optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.01, momentum=0.9)
-
-# # Decay LR by a factor of 0.1 every 7 epochs
-# exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-
-# model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=15)
-# torch.save(model_ft.state_dict(), "Dec10_r50_15ep_regression.pt")
-
-# visualize_model(model_ft)
-
-# # imshow(out, title="class list")
-
-def get_dataset(data_filepath):
-    data = np.load(data_filepath)
-
-    tensor_x = torch.Tensor(data['x'])
-    tensor_y = torch.Tensor(data['y'])
-
-    dataset = TensorDataset(tensor_x,tensor_y)
-
+    dataset = ConcatDataset(dataset_list)
     return dataset
 
 if __name__ == "__main__":
-    input_size = 8
-    hidden_size = 64
-    batch_first = True
-    batch_size = 52
-    # model = baselineLSTM(input_size,hidden_size,batch_size,batch_first)
-    # model = baselineGRU(input_size,hidden_size,batch_size,batch_first)
-    model = baselineResNet()
-    # model = baselineFCNLSTM(input_size,hidden_size,batch_size,batch_first)
+    batch_size = 32
+    output_nodes = 1
+    model = modifiedResNet(output_nodes)
 
-    training_dataset = get_dataset('eeg_dataset_training2.npz')
+    training_dataset = get_dataset('/media/matt/New Volume/ava/np_regress_files/',0)
     training_loader = DataLoader(dataset=training_dataset,batch_size=batch_size,shuffle=True)
 
-    validation_dataset = get_dataset('eeg_dataset_testing2.npz')
+    validation_dataset = get_dataset('/media/matt/New Volume/ava/np_regress_files/',1024)
     validation_loader = DataLoader(dataset=validation_dataset,batch_size=batch_size)
 
-    # PATH = 'baselineLSTM.pth'
-    # PATH = 'baselineGRU.pth'
-    PATH = 'baselineRNN.pth'
-    # PATH = 'baselineFCNLSTM.pth'
+    PATH = 'modifiedResNet.pt'
     train_model(model,PATH,training_loader,validation_loader)
 
